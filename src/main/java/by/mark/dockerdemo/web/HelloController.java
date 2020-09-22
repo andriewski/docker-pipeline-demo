fd71941a6153d5d8b5793c38475f6471641532db
Mark <plambiroo@gmail.com>
2020-09-22 13:27:07 +0300

    first commit

---
+++ src/main/java/by/mark/dockerdemo/web/HelloController.java
@@ -0,0 +1,13 @@
+package by.mark.dockerdemo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello, world!";
+    }
+}
